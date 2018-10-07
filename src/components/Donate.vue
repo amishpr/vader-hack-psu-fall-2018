6089d766677f3ab8a02b93bdbfb1b6412a7eb9a7
javacoldbrew <amish.prajapati@gmail.com>
2018-10-07 13:39:35 -0400

    Added Donate page

---
+++ src/components/Donate.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="row p-1">
+      <div class="col-2">
+        <label>First Name</label>
+      </div>
+      <div class="col-3">
+        <input class="form-control"/>
+      </div>
+      <div class="col-2">
+        <label>Last Name</label>
+      </div>
+      <div class="col-3">
+        <input class="form-control"/>
+      </div>
+    </div>
+    <div class="row p-1">
+      <div class="col-2">
+        <label>Credit Card</label>
+      </div>
+      <div class="col-10">
+        <input class="form-control"/>
+      </div>
+    </div>
+    <div class="row p-1">
+      <div class="col-2">
+        <label>CVC</label>
+      </div>
+      <div class="col-2">
+        <input class="form-control"/>
+      </div>
+      <div class="col-2">
+        <label>Expiration Date</label>
+      </div>
+      <div class="col-3">
+        <input class="form-control"/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-2">
+        <label>Address</label>
+      </div>
+      <div class="col-10">
+        <input class="form-control"/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-2">
+        <label></label>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "Donate"
+  }
+</script>
+
+<style scoped>
+
+</style>
