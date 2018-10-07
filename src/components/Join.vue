3b3a7749acfd9fadf06623920a315514f5fcda55
javacoldbrew <amish.prajapati@gmail.com>
2018-10-07 13:42:01 -0400

    Added Join page

--- src/components/Join.vue
+++ src/components/Join.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="container">
-  <table class="table table-bordered">
-    <tr>
-      <th>Name</th>
-      <th>Email</th>
-    </tr>
-    <tr v-for="v in volunteers">
-     <td>{{v.name}}</td>
-     <td>{{v.contact}}</td>
-    </tr>
-  </table>
+    <a>https://{{city.toLowerCase()}}.flood.slack.com</a>
+    <table class="table table-bordered">
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>
+      <tr v-for="v in volunteers">
+        <td>{{v.name}}</td>
+        <td>{{v.contact}}</td>
+      </tr>
+    </table>
     <button class="btn btn-success">Donate</button>
   </div>
 </template>
@@ -18,9 +19,12 @@
   export default {
     name: "Join",
     computed: {
-      volunteers () {
+      volunteers() {
         return this.$store.state.volunteers
-      }
+      },
+      city() {
+        return this.$store.state.city
+      },
     }
   }
 </script>
