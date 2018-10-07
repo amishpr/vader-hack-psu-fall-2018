9c729be7699b1ac69fed7f60f71adb67b07b3887
javacoldbrew <amish.prajapati@gmail.com>
2018-10-07 11:25:20 -0400

    Added Login page

---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row mb-1 mt-1">
+      <div class="col-2">
+        <label class="mr-1">Email: </label>
+      </div>
+      <div class="col-6">
+        <input class="form-control"placeholder="Email"/>
+      </div>
+    </div>
+    <div class="row mb-1">
+      <div class="col-2">
+        <label class="mr-1">Password: </label>
+      </div>
+      <div class="col-6">
+        <input class="form-control"placeholder="Password"/>
+      </div>
+    </div>
+    <router-link to="/home">
+    <button class="btn btn-primary">Login</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Login"
+  }
+</script>
+
+<style scoped>
+
+</style>
