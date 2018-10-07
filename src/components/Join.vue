ae7c7afff24e6f1a62f62689a84a025dcd79b858
javacoldbrew <amish.prajapati@gmail.com>
2018-10-07 11:25:04 -0400

    Added Join page

---
+++ src/components/Join.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+  <table class="table table-bordered">
+    <tr>
+      <th>Name</th>
+      <th>Email</th>
+    </tr>
+    <tr v-for="v in volunteers">
+     <td>{{v.name}}</td>
+     <td>{{v.contact}}</td>
+    </tr>
+  </table>
+    <button class="btn btn-success">Donate</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Join",
+    computed: {
+      volunteers () {
+        return this.$store.state.volunteers
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
